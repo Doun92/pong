2a9d82b6cd463e54abb826bd409ca5abfb7a4c7e
Doun <daniel.esco92@gmail.com>
2026-05-24 18:08:47 +0200

    Add background music to scene and game data
    Add background music resource and an AudioStreamPlayer2D node to the main scene (res://sons/background/dream-protocol-ping-pong-classic-arcade-game-116818.mp3) with autoplay enabled. Also create and attach an AudioStreamPlayer in scripts/gameData.gd on _ready(), preload the same audio stream, set volume, and start playback so the game plays background music at runtime.

--- scripts/gameData.gd
+++ scripts/gameData.gd
@@ -3,3 +3,12 @@ extends Node
 var game_type = ""
 
 var position_ball_y = 0
+
+var musique: AudioStreamPlayer
+
+func _ready():
+	musique = AudioStreamPlayer.new()
+	add_child(musique)
+	musique.stream = preload("res://sons/background/dream-protocol-ping-pong-classic-arcade-game-116818.mp3")
+	musique.volume_db = -20
+	musique.play()
